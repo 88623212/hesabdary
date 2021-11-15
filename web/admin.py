e272109ffb1529f3c7684364f67fe5138c06826f
hosein <hgodarzi@gmail.com>
2021-11-15 11:56:11 +0330

    '.gitignore_web/admin.py_web/models.py_web/migrations/0001_initial.py_web/migrations/0002_daramad.py___commit'

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Expense
+from .models import daramad
 # Register your models here.
+admin.site.register(Expense)
+admin.site.register(daramad)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,22 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, verbose_name='hosein')
+    def __str__(self):
+        return '%s - %s -%s'%(self.date ,self.text,self.amount)
+
+class daramad(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, verbose_name='hosein')
+    def __str__(self):
+        return '%s - %s -%s'%(self.date ,self.text,self.amount)        
